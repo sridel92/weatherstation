309198e71b3f0bc1b7c65cbc44ca6258fe175152
sridel92 <sridel@free.fr>
2021-03-21 09:09:59 +0100

    Add files via upload

---
+++ Code/esp8266-weather-station-color_sri0/settings.h
@@ -0,0 +1,93 @@
+/**The MIT License (MIT)
+ 
+ Copyright (c) 2018 by ThingPulse Ltd., https://thingpulse.com
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+#include <Arduino.h>
+#include <TZ.h>
+
+// Setup
+#define WIFI_SSID "PierreJoigneaux124"
+#define WIFI_PASS "LaPetiteCabane123"
+#define WIFI_HOSTNAME "SRID-weather-station-color"
+
+const int UPDATE_INTERVAL_SECS = 10 * 60; // Update every 10 minutes
+const int SLEEP_INTERVAL_SECS = 0;        // Going to sleep after idle times, set 0 for insomnia
+
+
+// OpenWeatherMap Settings
+// Sign up here to get an API key: https://docs.thingpulse.com/how-tos/openweathermap-key/
+String OPEN_WEATHER_MAP_APP_ID = "1bce1eab3c98dc879af9a1c235c919f3";
+/*
+Go to https://openweathermap.org/find?q= and search for a location. Go through the
+result set and select the entry closest to the actual location you want to display 
+data for. It'll be a URL like https://openweathermap.org/city/2657896. The number
+at the end is what you assign to the constant below.
+ */
+String OPEN_WEATHER_MAP_LOCATION_ID = "3036572";
+String DISPLAYED_CITY_NAME = "Asnieres sur Seine";
+/*
+Arabic -> ar, Bulgarian -> bg, Catalan -> ca, Czech -> cz, German -> de, Greek -> el,
+English -> en, Persian (Farsi) -> fa, Finnish -> fi, French -> fr, Galician -> gl,
+Croatian -> hr, Hungarian -> hu, Italian -> it, Japanese -> ja, Korean -> kr,
+Latvian -> la, Lithuanian -> lt, Macedonian -> mk, Dutch -> nl, Polish -> pl,
+Portuguese -> pt, Romanian -> ro, Russian -> ru, Swedish -> se, Slovak -> sk,
+Slovenian -> sl, Spanish -> es, Turkish -> tr, Ukrainian -> ua, Vietnamese -> vi,
+Chinese Simplified -> zh_cn, Chinese Traditional -> zh_tw.
+*/
+String OPEN_WEATHER_MAP_LANGUAGE = "fr";
+
+// Adjust according to your language
+const String WDAY_NAMES[] = {"Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"};
+const String MONTH_NAMES[] = {"Jan", "Fev", "Mar", "Avr", "Mai", "Jun", "Jul", "Aou", "Sep", "Oct", "Nov", "Dec"};
+const String SUN_MOON_TEXT[] = {"Sun", "Set", "Rise", "Moon", "Age", "Illum"};
+const String MOON_PHASES[] = {"Nouvelle", "Waxing Crescent", "1er quartier", "Waxing Gibbous",
+                              "Pleine", "Waning Gibbous", "Dernier quart", "Waning Crescent"};
+
+// pick one from https://github.com/esp8266/Arduino/blob/master/cores/esp8266/TZ.h
+#define TIMEZONE TZ_Europe_Zurich
+
+// values in metric or imperial system?
+const boolean IS_METRIC = true;
+
+// Change for 12 Hour/ 24 hour style clock
+bool IS_STYLE_12HR = false;
+
+// change for different NTP (time servers)
+#define NTP_SERVERS "0.ch.pool.ntp.org", "1.ch.pool.ntp.org", "2.ch.pool.ntp.org"
+// #define NTP_SERVERS "us.pool.ntp.org", "time.nist.gov", "pool.ntp.org"
+
+// August 1st, 2018
+#define NTP_MIN_VALID_EPOCH 1533081600
+
+// Pins for the ILI9341
+#define TFT_DC D2
+#define TFT_CS D1
+#define TFT_LED D8
+
+#define HAVE_TOUCHPAD
+#define TOUCH_CS D3
+#define TOUCH_IRQ  D4
+
+
+/***************************
+ * End Settings
+ **************************/
